8aa808b6ff5401778c6640c703a8f0a494222e7f
Helio Barolo <barolohelio@gmail.com>
2022-10-24 11:32:47 -0300

    Commit Inicial - Projeto cadastro simples/CRUD

---
+++ src/pages/index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Layout } from "../components/Layout";
+
+export default function Home() {
+  return (
+    <div className={`
+      flex justify-center items-center h-screen
+      bg-gradient-to-r from-blue-500 to-purple-500
+      text-white
+    `}>
+      <Layout 
+        title="Cadastro Simples - CRUD"
+      >
+        <span>Conteúdo</span>
+      </Layout>
+    </div>
+  )
+}
